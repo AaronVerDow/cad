ea1a4cc69011abd660a072f21a0a9d56850ed0cd
Aaron <aaron@verdow.com>
2014-05-06 00:24:53 +0000

    adding new firmware

---
+++ Marlin/build-cli/digipot_mcp4451.d
@@ -0,0 +1,10 @@
+build-cli/digipot_mcp4451.o: digipot_mcp4451.cpp Configuration.h \
+ Configuration_adv.h thermistortables.h Marlin.h fastio.h pins.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/variants/mega/pins_arduino.h \
+ MarlinSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h
